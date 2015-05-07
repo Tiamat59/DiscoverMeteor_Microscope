d7842066718fbaeed1c25190c6b7eba2e50e3989
tiamat <tiamat@toto.com>
2015-05-07 12:09:01 +0200

    Fin premiere etape

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,76 @@
-if (Posts.find().count() === 0){
-	Posts.insert({
-		title: 'Introducing Telescopt',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	});
-	
-	Posts.insert({
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	});
+// Données de préinstallation
+if (Posts.find().count() === 0) {
+  var now = new Date().getTime();
+
+  // Créer deux utilisateurs
+  var tomId = Meteor.users.insert({
+    profile: { name: 'Tom Coleman' }
+  });
+  var tom = Meteor.users.findOne(tomId);
+  var sachaId = Meteor.users.insert({
+    profile: { name: 'Sacha Greif' }
+  });
+  var sacha = Meteor.users.findOne(sachaId);
+
+  var telescopeId = Posts.insert({
+    title: 'Introducing Telescope',
+    userId: sacha._id,
+    author: sacha.profile.name,
+    url: 'http://sachagreif.com/introducing-telescope/',
+    submitted: new Date(now - 7 * 3600 * 1000),
+    commentsCount: 2,
+    upvoters: [],
+    votes: 0
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: "C'est un projet intéressant Sacha, est-ce-que je peux y participer ?"
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: 'Bien sûr Tom !'
+  });
 
   Posts.insert({
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	});
+    title: 'Meteor',
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://meteor.com',
+    submitted: new Date(now - 10 * 3600 * 1000),
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
+  });
+
+  Posts.insert({
+    title: 'The Meteor Book',
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://themeteorbook.com',
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
+  });
+
+  for (var i = 0; i < 10; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: new Date(now - i * 3600 * 1000 + 1),
+      commentsCount: 0,
+      upvoters: [],
+      votes: 0
+    });
+  }
 }
